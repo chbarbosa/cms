134349ccff74c3753ec6afa7d726db7275b8a4a5
CH Barbosa <carloshenriquefb@gmail.com>
2018-05-09 22:48:11 -0300

    handlebars configuration included

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const exphds = require('express-handlebars')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', exphds({defaultLayout: 'home'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res)=>{
+
+    res.render('home/index');
+
+});
 
 app.listen(4500, ()=>{
     console.log(`Listening on port 4500`);
