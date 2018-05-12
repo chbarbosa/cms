269c85484129db3eaa072dff9b52d49febe0a989
CH Barbosa <carloshenriquefb@gmail.com>
2018-05-12 08:43:03 -0300

    refactoring routes file added

--- app.js
+++ app.js
@@ -8,26 +8,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphds({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res)=>{
+const main = require('./routes/home/main');
 
-    res.render('home/index');
-
-});
-app.get('/about', (req, res)=>{
-
-    res.render('home/about');
-
-});
-app.get('/login', (req, res)=>{
-
-    res.render('home/login');
-
-});
-app.get('/register', (req, res)=>{
-
-    res.render('home/register');
-
-});
+app.use('/', main);
 
 app.listen(4500, ()=>{
     console.log(`Listening on port 4500`);
