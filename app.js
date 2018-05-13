05802f147349a3140dc8d21a62bccd827431eb16
CH Barbosa <carloshenriquefb@gmail.com>
2018-05-13 12:39:48 -0300

    admin setup

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphds({defaultLayout: 'home'}));
 app.set('view engine', 'handlebars');
 
-const main = require('./routes/home/main');
+const home = require('./routes/home/index');
+const admin = require('./routes/home/index');
 
-app.use('/', main);
+app.use('/', home);
+app.use('/admin', admin);
 
 app.listen(4500, ()=>{
     console.log(`Listening on port 4500`);
