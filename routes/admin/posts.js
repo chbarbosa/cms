cfff8833a549adcc7e0fb317d553e562dc81f322
CH Barbosa <carloshenriquefb@gmail.com>
2018-05-24 18:37:03 -0300

    posts list page added

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const post = require('../../models/Post');
+const Post = require('../../models/Post');
 
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout = 'admin';
@@ -8,7 +8,9 @@ router.all('/*',(req,res,next)=>{
 });
 
 router.get('/', (req, res)=>{
-    res.send('It works');
+    Post.find({}).then(posts=>{
+        res.render('admin/posts', {posts: posts});
+    });
 });
 
 router.get('/create', (req, res)=>{
@@ -23,14 +25,17 @@ router.post('/create', (req, res)=>{
         allowComments = true;
     }
 
-    Post({
+    const newPost = new Post({
         title: req.body.title,
         status: req.body.status,
         allowComments: allowComments,
         body: req.body.body
 
     });
-    console.log(req.body);
+
+    newPost.save().then(savedPost =>{
+        res.redirect('/admin/posts');
+    });
 });
 
 module.exports = router;
