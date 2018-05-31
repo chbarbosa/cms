245be1179119cf459865fb8aaee361a86107bfd2
CH Barbosa <carloshenriquefb@gmail.com>
2018-05-31 14:45:39 -0300

    delete option for posts added

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -69,4 +69,15 @@ router.put('/edit/:id', (req, res)=>{
     //Not found
 });
 
+
+
+router.delete('/:id', (req, res)=>{
+    Post.remove({_id: req.params.id}).then(result => {
+        res.redirect('/admin/posts');
+    });
+    //if not found
+    //if not removed?
+});
+
+
 module.exports = router;
