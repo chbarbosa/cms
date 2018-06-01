5861455374ebe316001b4c165f383b5c6bf0395c
CH Barbosa <carloshenriquefb@gmail.com>
2018-06-01 17:43:06 -0300

    random data generation with faker added

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const faker = require('faker');
+const Post = require('../../models/Post');
 
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout = 'admin';
@@ -8,6 +10,15 @@ router.all('/*',(req,res,next)=>{
 
 router.get('/', (req, res)=>{
     res.render('admin/index');
+});
+
+router.post('/generate-fake-posts', (req, res)=>{
+
+    for (let index = 0; index < param.body.amount; index++) {
+        
+        
+    }
+
 });
 
 module.exports = router;
