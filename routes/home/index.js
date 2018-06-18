4707fd42e75a29ccce9d4e4be0792606687ffe8e
CH Barbosa <carloshenriquefb@gmail.com>
2018-06-18 18:42:55 -0300

    post list added to main page

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../../models/Post');
 
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout = 'home';
@@ -8,7 +9,11 @@ router.all('/*',(req,res,next)=>{
 
 router.get('/', (req, res)=>{
 
-    res.render('home/index');
+    Post.find({}).then(posts => {
+        res.render('home/index', {posts: posts});
+    });
+
+    //a missing catch error here
 
 });
 router.get('/about', (req, res)=>{
