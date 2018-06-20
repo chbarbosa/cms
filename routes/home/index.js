3e2d3236a20a1d4319822b13554e31f5f0f88bbe
CH Barbosa <carloshenriquefb@gmail.com>
2018-06-20 18:52:00 -0300

    post list of home finished

--- routes/home/index.js
+++ routes/home/index.js
@@ -31,5 +31,13 @@ router.get('/register', (req, res)=>{
     res.render('home/register');
 
 });
+router.get('/post/:id', (req, res)=>{
+
+    Post.findOne({_id: req.param._id})
+    .then(post => {
+        res.render('home/post', {post: post});
+    });
+
+});
 
 module.exports = router;
