d0a6b8bc099a6bb203dfc495d2b2f45602ab86d4
CH Barbosa <carloshenriquefb@gmail.com>
2018-06-24 08:39:52 -0300

    Category added to the project

---
+++ routes/admin/categories.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const faker = require('faker');
+const Category = require('../../models/Category');
+
+router.all('/*',(req,res,next)=>{
+    req.app.locals.layout = 'admin';
+    next();
+});
+
+router.get('/', (req, res)=>{
+    res.render('admin/categories/index');
+});
+
+
+module.exports = router;
