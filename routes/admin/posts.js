95137163d5b1f0848eecebae811e7c3ba3a66580
CH Barbosa <carloshenriquefb@gmail.com>
2018-07-04 20:40:33 -0300

    cetegory selection added on post form

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post');
+const Category = require('../../models/Category');
 const { isEmpty, uploadDir } = require('../../helpers/upload-helper');
 const fs = require('fs');
 
@@ -16,7 +17,9 @@ router.get('/', (req, res)=>{
 });
 
 router.get('/create', (req, res)=>{
-    res.render('admin/posts/create');
+    Category.find({}).then(categories => {
+        res.render('admin/posts/create', {categories: categories});
+    });
 });
 
 router.post('/create', (req, res)=>{
