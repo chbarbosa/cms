d3746fa9dd26db8691c5895f6b5d0bbc0ac045cf
CH Barbosa <carloshenriquefb@gmail.com>
2018-07-03 21:37:10 -0300

    categories list sidebar

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../../models/Post');
+const Category = require('../../models/Category');
 
 router.all('/*',(req,res,next)=>{
     req.app.locals.layout = 'home';
@@ -10,7 +11,9 @@ router.all('/*',(req,res,next)=>{
 router.get('/', (req, res)=>{
 
     Post.find({}).then(posts => {
-        res.render('home/index', {posts: posts});
+        Category.find({}).then(categories => {
+            res.render('home/index', {posts: posts, categories: categories});
+        });
     });
 
     //a missing catch error here
@@ -35,7 +38,10 @@ router.get('/post/:id', (req, res)=>{
 
     Post.findOne({_id: req.params.id})
     .then(post => {
-        res.render('home/post', {post: post});
+
+        Category.find({}).then(categories => {
+            res.render('home/post', {post: post, categories: categories});
+        });
     });
 
 });
