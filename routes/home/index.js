425ab2c86508735f7c0b51716e73e21961cc5209
CH Barbosa <carloshenriquefb@gmail.com>
2018-07-14 09:19:36 -0300

    login still incomplete password comparation added

--- routes/home/index.js
+++ routes/home/index.js
@@ -36,6 +36,18 @@ router.get('/login', (req, res)=>{
 
 //APP Login
 passport.use(new LocalStrategy({usernameField: 'email'},(email, password, done)=> {
+    User.findOne({email:email}).then(user=>{
+        if(!user) return done(null, false, {message: 'no user found'});
+        bcrypt.compare(password, user.password, (err, matched)=>{
+            if (err) return err;
+            if (matched) {
+                return done(null, user);                
+            } else {
+                return done(null, false, {message:'password is incorrect'});
+                
+            }
+        });
+    });
     console.log(password);
 }));
 
