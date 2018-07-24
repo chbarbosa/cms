4fd6f67f147c9d161bb0ae1fc9ae9ea8b62bc66d
CH Barbosa <carloshenriquefb@gmail.com>
2018-07-23 22:20:54 -0300

    comments admin list

--- routes/admin/comments.js
+++ routes/admin/comments.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const Post = require('../../models/Post');
 const Comment = require('../../models/Comment');
 
+router.all('/*',(req,res,next)=>{
+    req.app.locals.layout = 'admin';
+    next();
+});
+
+router.get('/', (req, res)=>{
+    res.render('admin/comments');
+});
+
 router.post('/', (req, res)=>{
 
     Post.findOne({_id: req.body.id}).then(post=>{
