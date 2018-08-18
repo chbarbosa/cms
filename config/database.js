2ca52f7c7206fa9320248218947e2df399a35100
CH Barbosa <carloshenriquefb@gmail.com>
2018-08-18 11:47:49 -0300

    first commit

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports= {
-    mongoDbUrl: 'mongodb://localhost:27017/cms'
+    //mongoDbUrl: 'mongodb://localhost:27017/cms'
+    mongoDbUrl: 'mongodb://cms-user:cms321@ds125872.mlab.com:25872/cmsofch'
 }
